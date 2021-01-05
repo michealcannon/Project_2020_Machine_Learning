3f19a7119788ea775c6ee27dd94134554f911869
micheal cannon <cannonmicheal@hotmail.com>
2021-01-05 21:27:42 +0000

    code that ouputs function into text box on button click

--- web_app.py
+++ web_app.py
@@ -11,6 +11,11 @@ app = fl.Flask(__name__)
 def home():
   return app.send_static_file('index.html')
 
+# Add root route.
+@app.route('/power')
+def power():
+  return {"value": np.random.normal()}
+
 # TO RUN IN WINDOWS:
 # set FLASK_APP=rando.py
 # python -m flask run
