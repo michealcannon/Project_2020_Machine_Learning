2729180df5341d9534b5d457d33c178fd1ab2c85
micheal cannon <cannonmicheal@hotmail.com>
2021-01-07 10:24:02 +0000

    added code to send user input to power function to allow power output be displayed on web page

--- web_app.py
+++ web_app.py
@@ -8,7 +8,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 # neural neworks
 import tensorflow.keras as kr
-
+from flask import Flask, jsonify, request, abort, render_template, redirect, url_for, session, flash
 # load model saved as .h5 file
 # https://machinelearningmastery.com/save-load-keras-deep-learning-models/#:~:text=Keras%20separates%20the%20concerns%20of,different%20formats%3A%20JSON%20and%20YAML.
 model = kr.models.load_model('model.h5')
@@ -22,11 +22,14 @@ def home():
   return app.send_static_file('index.html')
 
 # Add root route.
-@app.route('/power')
+@app.route('/power', methods=['POST', 'GET'])
 def power():
-  value = model.predict([5])
-  return {"value" : str(value[0][0])}  # have to extract the value from the matrix here and change to string to allow json return
-
+  input = fl.request.args.get('input')
+  x = float(input)
+  value = model.predict([x])
+  output = str(value[0][0])     # have to extract the value from the matrix here and change to string to allow json return
+  return {"html" : str(value[0][0])}  # return as html... code adapted from https://stackoverflow.com/questions/50042492/call-flask-function-from-form-and-display-results-on-same-page
+  
 # TO RUN IN WINDOWS:
 # set FLASK_APP=rando.py
 # python -m flask run
