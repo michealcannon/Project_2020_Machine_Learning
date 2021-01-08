1ad7f22ad3b8abdcbb62fc4e153a3fc54f0ede3f
micheal cannon <cannonmicheal@hotmail.com>
2021-01-08 19:56:54 +0000

    small changes to code: error message, rounding, comments- a requirements file

--- web_app.py
+++ web_app.py
@@ -9,8 +9,10 @@ import matplotlib.pyplot as plt
 # neural neworks
 import tensorflow.keras as kr
 from flask import Flask, jsonify, request, abort, render_template, redirect, url_for, session, flash
+
+
 # load model saved as .h5 file
-# https://machinelearningmastery.com/save-load-keras-deep-learning-models/#:~:text=Keras%20separates%20the%20concerns%20of,different%20formats%3A%20JSON%20and%20YAML.
+#  code  from here https://machinelearningmastery.com/save-load-keras-deep-learning-models/#:~:text=Keras%20separates%20the%20concerns%20of,different%20formats%3A%20JSON%20and%20YAML.
 model = kr.models.load_model('model.h5')
 
 # Create a new web app.
@@ -21,18 +23,18 @@ app = fl.Flask(__name__)
 def home():
   return app.send_static_file('index.html')
 
-# Add root route.
+# Add /power route.
 @app.route('/power', methods=['POST', 'GET'])
 def power():
-  input = fl.request.args.get('input')
-  x = float(input)
-  value = model.predict([x])
-  output = str(value[0][0])     # have to extract the value from the matrix here and change to string to allow json return
-  return {"html" : output}  # return as html... code adapted from https://stackoverflow.com/questions/50042492/call-flask-function-from-form-and-display-results-on-same-page
-  
-# TO RUN IN WINDOWS:
-# set FLASK_APP=rando.py
-# python -m flask run
+  input = fl.request.args.get('input')  # input from web page assigned to variable input
+  x = float(input)      # change input to float
+  if x <=24:                    # only allow inputs less than/equal to 24 as model predicts lots of zero values after this
+    value = model.predict([x])        # apply model to user input
+    output = str(round(value[0][0], 3))     # have to extract the value from the matrix here and change to string to allow json return - round to 3 dp
+    return {"html" : output}  # return as html... code adapted from https://stackoverflow.com/questions/50042492/call-flask-function-from-form-and-display-results-on-same-page
+  else:
+    return {"html": "Reliable outputs can not be provided for inputs greater than 24"}  # error message
+
 
 
 
